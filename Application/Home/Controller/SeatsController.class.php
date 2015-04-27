ee42eb2ce924c5def4b355a1843653b4b8a819cc
rainstorm0506 <2875128903@qq.com>
2015-04-27 23:26:21 +0800

    订座的时候，通过Ajax查询该时间段是否已经“订座”

--- Application/Home/Controller/SeatsController.class.php
+++ Application/Home/Controller/SeatsController.class.php
@@ -21,12 +21,21 @@ class SeatsController extends \Think\Controller{
         $reservation_model=M("reservation");
         $seats_id=I("get.seats_id");
         if(IS_POST){    //保存订座数据
-            $data=I("post.");
+            $datas= $_POST;
+            
+            $data['seats_id']=$datas['seats_id'];
+            $data['custom_name']=$datas['custom_name'];
+            $data['phone']=$datas['phone'];
+            $data['meals_number']=$datas['meals_number'];
+            $data['meals_date']=$datas['meals_date'];
+            $data['meals_time']=$datas['meals_time'];
+            
             $id=$reservation_model->add($data);
             if($id){
-                $this->success("在线订座成功!",U('finishSeats',array("reservation_id"=>$id)));
+                //$this->success("在线订座成功!",U('finishSeats',array("reservation_id"=>$id)));
+                $this->ajaxReturn($id);
             }else{
-                $this->error("在线订座失败");
+                $this->ajaxReturn(0);
             }
         }else{  //展示订座页面
             //根据座位ID得倒记录
@@ -44,5 +53,20 @@ class SeatsController extends \Think\Controller{
         $item=$reservation_model->where("id={$reservation_id}")->find();
         $this->assign("row",$item);
         $this->display("seat_wc");
+    }
+    //判断 某一桌位在某个时间点 是否存在那么一条记录
+    public function searshItem(){
+        $reservation_model=M("reservation");
+        
+        $seats_id=$_POST["seats_id"];
+        $meals_date=$_POST["meals_date"];
+        $meals_time=$_POST["meals_time"];
+        //通过上面的三个变量，组装查询条件
+        $row=$reservation_model->where(array("seats_id"=>$seats_id,"meals_date"=>$meals_date,"meals_time"=>$meals_time))->find();
+        if($row){
+            $this->ajaxReturn(1);
+        }else{
+            $this->ajaxReturn(0);
+        }
     }
 }
